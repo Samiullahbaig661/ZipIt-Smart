963592105b3e0fa8cd1ea1f75093c8998f3dda99
Samiullahbaig661 <samiullahbaig661@gmail.com>
2025-12-06 00:01:11 +0500

    Folder compression done

---
+++ CompressorAndDecompressor/CompressorAndDecompressor/Compress.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompressorAndDecompressor
+{
+    public partial class Compress: Form
+    {
+        public Compress()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolderPath.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string folderPath = txtFolderPath.Text;
+
+            if (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
+            {
+                MessageBox.Show("Please select a valid folder first.");
+                return;
+            }
+
+            using (var saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Filter = "Huffman Archive (*.huff)|*.huff";
+                saveDialog.Title = "Save Compressed Folder";
+                saveDialog.FileName = Path.GetFileName(folderPath) + ".huff";
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var compressor = new CompressorAndDecompressor.FolderCompressor();
+                        compressor.CompressFolder(folderPath, saveDialog.FileName);
+
+                        MessageBox.Show("Folder compressed successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error while compressing: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CompressorAndDecompressor/CompressorAndDecompressor/FolderCompressor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressorAndDecompressor
+{
+    internal class FolderCompressor
+    {
+        public void CompressFolder(string inputFolderPath, string outputArchivePath)
+        {
+            // saari files (sirf top level; subfolders nahi)
+            string[] files = Directory.GetFiles(inputFolderPath, "*.*", SearchOption.TopDirectoryOnly);
+
+            using (var fs = new FileStream(outputArchivePath, FileMode.Create))
+            using (var bw = new BinaryWriter(fs))
+            {
+                // total files likho
+                bw.Write(files.Length);
+
+                foreach (var file in files)
+                {
+                    // original bytes
+                    byte[] originalBytes = File.ReadAllBytes(file);
+
+                    // yahan tumhara Huffman / compression function call hoga
+                    byte[] compressedBytes = HuffmanService.Compress(originalBytes);
+
+                    string fileName = Path.GetFileName(file);
+
+                    // metadata
+                    bw.Write(fileName.Length);          // int
+                    bw.Write(fileName);                 // string
+                    bw.Write(originalBytes.Length);     // int
+                    bw.Write(compressedBytes.Length);   // int
+
+                    // compressed data
+                    bw.Write(compressedBytes);
+                }
+            }
+        }
+    }
+
+    // yeh sirf example hai – tumhari team ka actual Huffman code yahan use hoga
+    public static class HuffmanService
+    {
+        public static byte[] Compress(byte[] inputBytes)
+        {
+            // TODO: yahan actual huffman compression lagana hai
+            return inputBytes; // abhi ke liye dummy
+        }
+    }
+
+}
